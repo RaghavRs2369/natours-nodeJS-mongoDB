870981feaf82e4d312cc50443616419dbc36321a
RaghavRs2369 <raghavendran1999@gmail.com>
2025-03-30 11:49:55 +0530

    Improving Read Performance With Indexes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,14 +4,12 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
-// POST /tour/234fde4/reviews
-// GET /tour/234fde4/reviews
+router.use(authController.protect);
 
 router
   .route('/')
   .get(reviewController.getAllReviws)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview,
@@ -20,7 +18,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview,
+  )
+  .delete(
+    authController.restrictTo('admin', 'user'),
+    reviewController.deleteReview,
+  );
 
 module.exports = router;
